c1ad4009736a6f48a70b9dc96de61caf365987e9
Leonardo <someleo@hotmail.com>
2019-08-22 10:29:02 -0300

    Terminando a estilização dos components

--- src/components/BitcoinVol.js
+++ src/components/BitcoinVol.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BitcoinVol = () => <small><span className="vol">500</span>bitcoins negociados nas ultimas 24hrs</small>
+const BitcoinVol = () => <small class="title"><span className="vol">500</span>bitcoins negociados nas ultimas 24hrs</small>
 
 export default BitcoinVol;
 
